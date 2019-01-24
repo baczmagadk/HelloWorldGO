3ca3aaf4b5fd8fe4c728aed04c5d11f4a5a22653
Daniel Baczmaga <baczmagadk@g.cofc.edu>
2019-01-24 12:34:33 -0500

    Create variables.go && Create variables

---
+++ variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println("a =", a)
+	fmt.Println()
+
+	var b, c  int= 1, 2
+	fmt.Println("b =", b)
+	fmt.Println("c =", c)
+	fmt.Println("b + c =", b + c)
+	fmt.Println()
+
+	var d bool= true
+	fmt.Println("d =", d)
+	fmt.Println()
+
+	var e int
+	fmt.Println("e =", e)
+	fmt.Println()
+
+	f := "short"
+	fmt.Println("f =", f)
+
+}
