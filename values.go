db1037f2021dbcda9dcc5f472a39b87e9577e54f
Daniel Baczmaga <baczmagadk@g.cofc.edu>
2019-01-24 12:22:37 -0500

    Create values.go

---
+++ values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Practice with GO Values")
+	fmt.Println()
+	fmt.Println("50 + 50 =", 50 + 50)
+	fmt.Println("45.0 / 3.0", 45.0 / 3.0)
+
+	fmt.Println(true && false, "false")
+	fmt.Println(true || false, "true")
+	fmt.Println(!true, "false")
+
+}
